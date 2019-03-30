42b6b773633e8c41d514009aaa8e7780d373ced5
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2019-03-30 18:50:17 -0400

    Add files via upload

---
+++ base.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+
+const defaultUser = {
+    "id": "",
+    "wins":0,
+    "losses":0,
+    "level":0,
+    "xp":0
+}
+
+client.on('ready', () => {
+    console.log("Bot ready.");
+    //client.user.setActivity("Working on things... DO NOT ATTEMPT TO USE.");
+    client.user.seta
+    client.user.setActivity(`{client.guilds.size} guilds insult eachother | help`, "WATCHING");
+});
+
+
+
+client.login(process.env.TOKEN);
