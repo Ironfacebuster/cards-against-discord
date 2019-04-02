8ea940dad190668ae8f71da1fd2f154001619ff9
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2019-04-02 17:42:12 -0400

    Add files via upload

--- base.js
+++ base.js
@@ -257,7 +257,7 @@ async function leave_room (_author, _message) {
             if(currentRooms[_roomindex].members[g]._id != _author.id) {
                 var _tempuser = client.fetchUser(currentRooms[_roomindex].members[g]._id);
                 _tempuser.then(function(_user) {
-                    _user.send(`${_author.username} joined your game.`);
+                    _user.send(`${_author.username} left your game.`);
                 });            
             }
         }
