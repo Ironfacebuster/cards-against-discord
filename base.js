d93aedfb9d25077fa9adf063cd0cf2db4c6f3236
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2019-04-01 21:03:54 -0400

    Add files via upload

--- base.js
+++ base.js
@@ -197,7 +197,7 @@ function join_room (_roomcode, _author, _message) {
         _message.reply("You're already in a game.");
     } else {
         const _room = currentRooms.findIndex(function(r){
-            console.log(r);
+            console.log(r.room_code + ", " + _roomcode);
             console.log(r.room_code.trim() == _roomcode.toString().trim());
             return r.room_code.trim() == _roomcode.toString().trim()
         });
