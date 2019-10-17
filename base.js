199d8986f79115f1b9a88ca5ac9712b6ab9f8564
CardsAgainstDiscord <53099257+CardsAgainstDiscord@users.noreply.github.com>
2019-10-17 19:05:12 -0400

    Add files via upload

--- base.js
+++ base.js
@@ -217,7 +217,7 @@ async function restart_bot(time,author) {
 
 async function list_rooms (author) {
     for(var i = 0; i < currentRooms.length; i++){
-        var content = JSON.stringify("`" + currentRooms[i]) + "`";
+        var content = "`" + JSON.stringify(currentRooms[i]) + "`";
         author.send(content);
     }
 }
